b8c045214e25ebb4895a1ee27e661c8f11971740
Kevin <k.lassauniere@live.fr>
2022-03-19 20:51:33 +0100

    Add user input for grid size; modify css to auto adjust when needed

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-function createBlock() {
-    const container = document.getElementById('grid-container');
+// globals
+const container = document.getElementById('grid-container');
 
-    // Create 16 div blocks
-    for (let i = 0; i < 16 * 16; i++) {
+function createGrid() {
+
+    const blocksPerLine = parseInt(prompt("How many blocks per line would you like?"), "10");
+
+    // input check
+    if (blocksPerLine < 1 || blocksPerLine > 100) {
+        alert('Wrong input. Enter a number between 1 and 100 please');
+        createGrid();
+    }
+
+    const sizeBlock = calculBlockSize(blocksPerLine);
+    
+    // Create the grid
+    for (let i = 0; i < blocksPerLine ** 2; i++) {
 
         const block = document.createElement('div');
         block.setAttribute('class', 'grid-block');
+        block.setAttribute('style', `width: ${sizeBlock}px; height: ${sizeBlock}px`);
         
         container.append(block);
     }
 }
 
-createBlock();
-
 function clearGrid() {
-    const container = document.getElementById('grid-container');
 
     const blocks = document.querySelectorAll('.grid-block');
     container.remove(blocks);
@@ -22,3 +32,22 @@ function clearGrid() {
 
 const reset = document.querySelector("button");
 reset.addEventListener('click', clearGrid);
+
+function calculBlockSize(blocksPerLine) {
+
+    // Calcul size of 1 block using container maxWidth
+    const maxWidth = container.offsetWidth;
+
+    const sizeBlock = Math.floor(maxWidth / blocksPerLine);
+
+    // Needed because of the sizeBlock round down
+    function adjustMaxWidth() {
+        const newMaxWidth = blocksPerLine * sizeBlock;
+        container.setAttribute('style', `max-width: ${newMaxWidth}px`);
+    }
+    adjustMaxWidth();
+
+    return sizeBlock;
+}
+
+createGrid();
